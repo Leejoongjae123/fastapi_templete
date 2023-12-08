442bbc234e6544dc2637358ccd6baf99e993d4cd
ljj33 <ljj3347@naver.com>
2023-12-08 21:47:05 +0900

    2nd commit

--- config.py
+++ config.py
@@ -1,13 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from urllib.parse import quote
 
 user = "admin" #대표님
 pwd = "dlwndwo2"
-host = "exgen.cpmqglgzxg0p.ap-northeast-2.rds.amazonaws.com"
+host = "database-2.crbwjwvrwwze.ap-northeast-2.rds.amazonaws.com"
 port = 3306
 
-DATABASE_URL = 'sqlite:///./test.db'
+DATABASE_URL = f'mysql+pymysql://{user}:{quote(pwd)}@{host}:{port}/information?charset=utf8mb4'
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush = False, bind=engine)
--- models.py
+++ models.py
@@ -1,9 +1,10 @@
 from sqlalchemy import  Column, Integer, String
 from config import Base
-
+from config import engine
 class Book(Base):
     __tablename__ ="book"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String)
-    description = Column(String)
+    title = Column(String(100))
+    description = Column(String(100))
+
